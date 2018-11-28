97196c3d765472e838260b433cd7058bf0ce77fa
bry-an <bbyunis@gmail.com>
2018-11-27 21:21:26 -0700

    test for passing data to vuserprofile

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
-        UserView You
+        <v-user-profile 
+            :user="user" />
+        <v-user-search-form />
     </div>
 </template>
 
 <script>
+import VUserProfile from "@/components/VUserProfile"
+import VUserSearchForm from "@/components/VUserSearchForm"
 export default {
-    name: "UserView"
+    name: "UserView", 
+    components: {
+        VUserProfile, 
+        VUserSearchForm
+    }
 }
 </script>
--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -1,11 +1,48 @@
-import { shallowMount } from "@vue/test-utils"
-import UserView from "@/views/UserView"
+import { shallowMount } from '@vue/test-utils'
+import UserView from '@/views/UserView'
+import VUserSearchForm from '@/components/VUserSearchForm'
+import VUserProfile from '@/components/VUserProfile'
 
-describe("UserView", () => {
-    it("renders the component", () => {})
-    //arrange
-    const wrapper = shallowMount(UserView) // render only the first level of dependencies
+describe('UserView', () => {
+  const build = () => {
+    const wrapper = shallowMount(UserView, {
+        data: () => ({
+            user: {}
+        })
+    })
 
+    return {
+      wrapper,
+      userSearchForm: () => wrapper.find(VUserSearchForm),
+      userProfile: () => wrapper.find(VUserProfile)
+    }
+  }
+
+  it('renders the component', () => {
+    // arrange
+    const { wrapper } = build()
     // assert
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('renders main child components', () => {
+    // arrange
+    const { userSearchForm, userProfile } = build()
+
+    // assert
+    expect(userSearchForm().exists()).toBe(true)
+    expect(userProfile().exists()).toBe(true)
+  })
+
+  it('passes a binded user prop to user profile component', () => {
+      // arrange
+      const { wrapper, userProfile } = build()
+      wrapper.setData({
+          user: {
+              name: 'Bryan'
+          }
+      })
+      //assert
+      expect(userProfile().vm.user).toBe(wrapper.vm.user)
+  })
 })
