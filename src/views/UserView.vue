7da1d720c9bc9c5f6a38769a55f21488aace44a5
bry-an <bbyunis@gmail.com>
2018-11-28 22:27:00 -0700

    passing in user data to userprofile from store

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -9,11 +9,17 @@
 <script>
 import VUserProfile from "@/components/VUserProfile"
 import VUserSearchForm from "@/components/VUserSearchForm"
+import { mapState } from 'vuex'
 export default {
     name: "UserView", 
     components: {
         VUserProfile, 
         VUserSearchForm
+    }, 
+    computed: {
+        ...mapState({
+            user: 'user'
+        })
     }
 }
 </script>
